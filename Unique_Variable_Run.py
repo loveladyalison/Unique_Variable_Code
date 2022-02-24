e14f4bb67631fadf2955edb685a8e0ce8b62d26a
Alison Lovelady <100036976+loveladyalison@users.noreply.github.com>
2022-02-23 21:11:20 -0600

    Add files via upload

---
+++ Unique_Variable_Run.py
@@ -0,0 +1,22 @@
+
+month = 2
+day = 22
+year = 2022
+appointment_hour = 2 #PM
+appointment_minute= 22 #Happy 2-Palindrome Day
+
+patient_name = 'Madyson'
+patient_age = 10
+patient_height = 55 #inches
+patient_weight = 45 #pounds
+patient_symptoms = 'trouble focusing on school work and visual sensory overload' 
+
+patient_diagnosis = 'ADHD'
+patient_medication = 'vyvanse'
+patient_medication_quantity = 'once a day'
+patient_medication_time = 'in the morning'
+
+print(f"{patient_name}, age {patient_age}, came into the office to see Doctor Who on {appointment_hour}:{appointment_minute} PM on the {day}nd of the {month}nd month of {year}.")
+print(f"Upon inital evaluation {patient_name} weighed {patient_weight} pounds and was measured to be {patient_height} inches tall.")
+print(f"{patient_name}'s mother reported that {patient_name} was having {patient_symptoms}.")
+print(f"Doctor Who diagnosed {patient_name} with {patient_diagnosis} and was prescribed {patient_medication} to be given {patient_medication_quantity} {patient_medication_time}.")
